f200889d0513c967bfa483468df25c3e25aac0a7
huypvn <huypvn@sbg.vn>
2024-01-26 17:01:12 +0700

    Cài đặt Laravel

---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+    }
+
+    public function messages(): array 
+    {
+        return [
+            'email.required' => 'Bạn chưa nhập vào email.',
+            'email.email' => 'Email chưa đúng định dạng. Ví dụ: abd@gmail.com',
+            'password.required' => 'Bạn chưa nhập vào mật khẩu.'
+        ]
+    }
+}
